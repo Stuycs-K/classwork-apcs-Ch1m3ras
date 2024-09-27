4e890bedfb2dc03199852199c8db2c37f73ff9a1
Ch1m3ras <awesomecool237@gmail.com>
2024-09-27 13:54:27 -0400

    Fixed some unfinished comments as well as making an initial version of concatArray with test cases.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -26,8 +26,18 @@ public class MyArrays{
     return returnedArray;
   }
 
-  public static int[] concatArray(int[] ary1, int[] ary 2){
-    
+  public static int[] concatArray(int[] ary1, int[] ary2){
+    int[] returnedArray = new int[ary1.length + ary2.length];
+    int returnedArrayIndex = 0;
+    for(int i = 0; i < ary1.length; i++){
+      returnedArray[returnedArrayIndex] = ary1[i];
+      returnedArrayIndex++;
+    }
+    for(int i = 0; i < ary2.length; i++){
+      returnedArray[returnedArrayIndex] = ary2[i];
+      returnedArrayIndex++;
+    }
+    return returnedArray;
   }
 
 
@@ -38,12 +48,17 @@ public class MyArrays{
     //Test case for arrayToString
     System.out.println(arrayToString(testingArray));
 
-    //Test cases for
+    //Test cases for returnCopy
     if(!(testingArray == returnCopy(testingArray))){
       System.out.println("Return Copy functions as it's supposed to.");
     }
     else{
       System.out.println("Return Copy does not function as it's supposed to. More testing required. ");
     }
+
+    //Test Cases for concatArray
+    int[] concat1 = {1, 5, 6, 8, 10};
+    int[] concat2 = {1, 1, 1, 56};
+    System.out.println(arrayToString(concat1) + " concatenated with " + arrayToString(concat2) + " returns " + arrayToString(concatArray(concat1, concat2)));
   }
 }
