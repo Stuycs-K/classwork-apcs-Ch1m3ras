862bec7fa1b74df486234564fff0ba6d3bfabd64
Ch1m3ras <awesomecool237@gmail.com>
2024-09-27 10:02:00 -0400

    Added arrayToString, ready for testing purposes.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,5 +1,22 @@
 public class MyArrays{
+
+  public static String arrayToString(int[] nums){
+    String returnedString = "[";
+    for(int i = 0; i < nums.length; i++){
+      if(i == nums.length - 1){
+        returnedString = returnedString + nums[i];
+      }
+      else{
+        returnedString = returnedString + nums[i] + ", ";
+      }
+    }
+    returnedString = returnedString + "]";
+    return returnedString;
+  }
+
   public static void main(String[] args){
     System.out.println("Hello World");
+    int[] testingArray = {1,2,3,4,5,56};
+    System.out.println(arrayToString(testingArray));
   }
 }
