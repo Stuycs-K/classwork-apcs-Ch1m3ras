dde9c5aa3572c0d303cfaec88b72abf93ac60620
Ch1m3ras <awesomecool237@gmail.com>
2024-09-30 14:46:58 -0400

    Added the main method as well as the class and fixed some issues with indents and spacing

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -1,55 +1,63 @@
-//1. Write both your names + emails at the top of the document as a comment.\
-// lbenitez60@stuy.edu Leonardo Benitez
-// alexanderc367@nycstudents.net Alexander Chen
+public class ArrayMethods{
+  //1. Write both your names + emails at the top of the document as a comment.\
+  // lbenitez60@stuy.edu Leonardo Benitez
+  // alexanderc367@nycstudents.net Alexander Chen
 
-//2. Copy your arrToString method from before.
-/**Return a String that represets the array in the format:
-* "[2, 3, 4, 9]"
-* Note the comma+space between values, and between values
-*/
-public static String arrToString(int[] nums){
-  //Starting bracket as every returned answer requires that.
-  String returnedString = "[";
-  for(int i = 0; i < nums.length; i++){
-    //Checking to see if it's the last term to make sure there is no hanging comma at the end.
-    if(i == nums.length - 1){
-      returnedString = returnedString + nums[i];
-    }
-    else{
-      returnedString = returnedString + nums[i] + ", ";
-    }
-  }
-  //Same logic as the starting bracket, all code will go through this point, and thus we add the ending bracket
-  returnedString = returnedString + "]";
-  return returnedString;
-}
-
-//3. Write arrToString, with a 2D array parameter.
-//Note: Different parameters are allowed with the same function name.
-/**Return a String that represets the 2D array in the format:
-  * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
-  * Note the comma+space between values, and between arrays
-  * You are encouraged to notice that you may want to re-use
-  * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
+  //2. Copy your arrToString method from before.
+  /**Return a String that represets the array in the format:
+  * "[2, 3, 4, 9]"
+  * Note the comma+space between values, and between values
   */
-public static String arrToString(int[][]ary){
-  //this should use arrToString(int[])
-  String result = "["
-  for (int i = 0; i < ary.length; i++){
-
+  public static String arrToString(int[] nums){
+    //Starting bracket as every returned answer requires that.
+    String returnedString = "[";
+    for(int i = 0; i < nums.length; i++){
+      //Checking to see if it's the last term to make sure there is no hanging comma at the end.
+      if(i == nums.length - 1){
+        returnedString = returnedString + nums[i];
+      }
+      else{
+        returnedString = returnedString + nums[i] + ", ";
+      }
+    }
+    //Same logic as the starting bracket, all code will go through this point, and thus we add the ending bracket
+    returnedString = returnedString + "]";
+    return returnedString;
   }
-  return "";
-}
 
-/*Return the sum of all of the values in the 2D array */
-public static int arr2DSum(int[][]nums){
-  //use a nested loop to solve this
-}
+  //3. Write arrToString, with a 2D array parameter.
+  //Note: Different parameters are allowed with the same function name.
+  /**Return a String that represets the 2D array in the format:
+    * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
+    * Note the comma+space between values, and between arrays
+    * You are encouraged to notice that you may want to re-use
+    * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
+    */
+  public static String arrToString(int[][]ary){
+    //this should use arrToString(int[])
+    String result = "[";
+    for (int i = 0; i < ary.length; i++){
+      arrToString(ary[i]);
+    }
+    result = result + "]";
+    return result;
+  }
 
-/**Rotate an array by returning a new array with the rows and columns swapped.
-  * You may assume the array is rectangular and neither rows nor cols is 0.
-  * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
-  */
-public static int[][] swapRC(int[][]nums){
-  return new int[1][1];
+  /*Return the sum of all of the values in the 2D array */
+  public static int arr2DSum(int[][]nums){
+    //use a nested loop to solve this
+    return 1;
+  }
+
+  /**Rotate an array by returning a new array with the rows and columns swapped.
+    * You may assume the array is rectangular and neither rows nor cols is 0.
+    * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+    */
+  public static int[][] swapRC(int[][]nums){
+    return new int[1][1];
+  }
+
+  public static void main(String[] args){
+    
+  }
 }
