a509bf6e49e54a09d884edb4cf6b93ab15ad39a4
Ch1m3ras <awesomecool237@gmail.com>
2024-11-07 10:06:21 -0500

    Added explaination for the speak change

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,9 @@
+//When we made the change to the getName accessor method, the new getName changed the result of speak, adding "The Mighty" to it.
+//This is because speak uses accessor methods in order to print the string.
+//If the accesor method changes, we will see a different result printed.
+
+
+
 public class Driver{
   public static void main(String[] args){
     Animal tester = new Animal("AAAAAAAAA", 5, "tester");
