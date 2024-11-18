d692e8a2f953240d0eee646eecdd7563313791c2
Ch1m3ras <awesomecool237@gmail.com>
2024-11-18 14:27:18 -0500

    Encountered errors with scanners not scanning the numbers out properly. Code associated with this is removed.

--- 2024-11-18-advent2016/Taxicab.java
+++ 2024-11-18-advent2016/Taxicab.java
@@ -9,27 +9,58 @@ public class Taxicab{
   //Sum the absolute value of north and east to get displacement.
   public static void main(String[] args){
     ArrayList<String> directions = new ArrayList<String>();
-    ArrayList<Integer> distances = new ArrayList<Integer>();
+    // ArrayList<Integer> distances = new ArrayList<Integer>();
     int distanceUp = 0;
     int distanceRight = 0;
     int direction = 0;
 
     try {
         File taxiInputs = new File("TaxicabInputs.txt");
+        // File taxiInputs2 = new File("TaxicabInputs.txt");
         Scanner inputDirection = new Scanner(taxiInputs);
-        Scanner inputDistances = new Scanner(taxiInputs);
+        // Scanner inputDistances = new Scanner(taxiInputs2);
         while(inputDirection.hasNext()){
+          String stuffInputed = inputDirection.next();
+          System.out.println(stuffInputed);
           directions.add(inputDirection.next());
         }
-        while(inputDistances.hasNextInt()){
-          distances.add(inputDistances.nextInt());
-        }
+        // while(inputDistances.hasNextInt()){
+        //   int stuffInputed = inputDistances.nextInt();
+        //   System.out.println(stuffInputed);
+        //   distances.add(stuffInputed);
+        // }
     }
     catch (FileNotFoundException e){
         System.out.println("File not found. Try again.");
         return;
     }
-    
+
+    for(int i = 0; i < directions.size(); i++){
+      if(directions.get(i).substring(0,1).equals("L")){
+        direction = direction - 90;
+        System.out.println(direction);
+      }
+      else if(directions.get(i).substring(0,1).equals("R")){
+        direction = direction + 90;
+        System.out.println(direction);
+      }
+
+    //   if(direction % 360 == 0){
+    //     distanceUp += distances.get(i);
+    //   }
+    //   else if(direction % 270 == 0){
+    //     distanceRight -= distances.get(i);
+    //   }
+    //   else if(direction % 180 == 0){
+    //     distanceUp -= distances.get(i);
+    //   }
+    //   else{
+    //     distanceRight += distances.get(i);
+    //   }
+    }
 
   }
+
+
+
 }
