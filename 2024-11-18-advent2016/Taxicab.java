10cbc1df9c765ba328b2686c0b5d82532d4cf764
Ch1m3ras <awesomecool237@gmail.com>
2024-11-18 10:13:01 -0500

    Added the try catch block for reading taxicabInputs and my main line of thinking in comments

---
+++ 2024-11-18-advent2016/Taxicab.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Taxicab{
+  //Plan: Use hasNext() and next() to seperate commands.
+  //Use a degree variable(with north being 0) and distanceDirection to measure how far they went in one direction or another (set south and west as negative and north and east as positive.)
+  //Sum the absolute value of north and east to get displacement.
+  public static void main(String[] args){
+    try {
+        File taxiInputs = new File("TaxicabInputs.txt");
+        Scanner inputs = new Scanner(file);
+
+    }
+    catch (FileNotFoundException e){
+        System.out.println("File not found. Try again.");
+        return//
+    }
+  }
+}
