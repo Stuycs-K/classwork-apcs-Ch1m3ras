8938383e36c4d7e3d3e1ad15aefc73a0a99a500e
Ch1m3ras <awesomecool237@gmail.com>
2024-11-18 14:02:50 -0500

    Added a new ArrayList for directions from the code.

--- 2024-11-18-advent2016/Taxicab.java
+++ 2024-11-18-advent2016/Taxicab.java
@@ -9,19 +9,27 @@ public class Taxicab{
   //Sum the absolute value of north and east to get displacement.
   public static void main(String[] args){
     ArrayList<String> directions = new ArrayList<String>();
+    ArrayList<Integer> distances = new ArrayList<Integer>();
+    int distanceUp = 0;
+    int distanceRight = 0;
+    int direction = 0;
 
     try {
         File taxiInputs = new File("TaxicabInputs.txt");
-        Scanner inputs = new Scanner(taxiInputs);
-        while(inputs.hasNext()){
-          directions.add(inputs.next());
+        Scanner inputDirection = new Scanner(taxiInputs);
+        Scanner inputDistances = new Scanner(taxiInputs);
+        while(inputDirection.hasNext()){
+          directions.add(inputDirection.next());
+        }
+        while(inputDistances.hasNextInt()){
+          distances.add(inputDistances.nextInt());
         }
     }
     catch (FileNotFoundException e){
         System.out.println("File not found. Try again.");
         return;
     }
-
+    
 
   }
 }
