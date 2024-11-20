5baeae18138b7932b19961e7003bddc32baa7865
Ch1m3ras <awesomecool237@gmail.com>
2024-11-19 21:51:39 -0500

    Made code that accurately counts the number of letters per a certain line of text

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -7,6 +7,7 @@ public class Day4{
 
   public static void main(String[] args){
     ArrayList<String> roomList = new ArrayList<String>();
+    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     int answer = 0;
 
     try{
@@ -22,7 +23,19 @@ public class Day4{
     }
 
     for(int i = 0; i < roomList.size(); i++){
-      System.out.println(roomList.get(i));
+      int[] amountAppeared = new int[26];
+      for(int i2 = 0; i2 < roomList.get(i).length() - 7; i2++){
+        //System.out.println(roomList.get(i).charAt(i2));
+        for(int i3 = 0; i3 < alphabet.length; i3++){
+          if(alphabet[i3] == roomList.get(i).charAt(i2)){
+            amountAppeared[i3] += 1;
+          }
+        }
+      }
+      for(int i4 = 0; i4 < amountAppeared.length; i4++){
+        System.out.println(amountAppeared[i4]);
+      }
+      System.out.println("\n\n");
     }
 
   }
