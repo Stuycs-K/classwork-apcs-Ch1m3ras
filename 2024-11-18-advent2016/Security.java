a5cd892ccbbf4cb1cb0ab221288ddc5f62c72c1c
Ch1m3ras <awesomecool237@gmail.com>
2024-11-19 21:05:43 -0500

    Added code react to different commands and change the current place in the keypad while also making sure it couldn't go out of bounds

--- 2024-11-18-advent2016/Security.java
+++ 2024-11-18-advent2016/Security.java
@@ -10,6 +10,7 @@ public class Security{
     ArrayList<String> commands = new ArrayList<String>();
     int currentPlaceHorizontal = 1;
     int currentPlaceVertical = 1;
+    String finalAnswer = "";
 
     try {
       File securityInputs = new File("Security.txt");
@@ -24,12 +25,49 @@ public class Security{
     }
 
     for(int i = 0; i < commands.size(); i++){
-      for(int i2 = 0; i < commands.get(i).length(); i2++){
-        System.out.println(commands.get(i).charAt(i2));
-      }
-      System.out.println("Part Over \n\n");
-    }
+      String currentStringUsed = commands.get(i);
+      for(int i2 = 0; i2 < currentStringUsed.length(); i2++){
+        char currentCommand = currentStringUsed.charAt(i2);
 
+        if(currentCommand == 'U'){
+          currentPlaceHorizontal -= 1;
+          //System.out.println(currentCommand + " x: " + currentPlaceHorizontal + " y: " + currentPlaceVertical);
+        }
+        else if(currentCommand == 'D'){
+          currentPlaceHorizontal += 1;
+          //System.out.println(currentCommand + " x: " + currentPlaceHorizontal + " y: " + currentPlaceVertical);
+        }
+        else if(currentCommand == 'L'){
+          currentPlaceVertical -= 1;
+          //System.out.println(currentCommand + " x: " + currentPlaceHorizontal + " y: " + currentPlaceVertical);
+        }
+        else if(currentCommand == 'R'){
+          currentPlaceVertical += 1;
+          //System.out.println(currentCommand + " x: " + currentPlaceHorizontal + " y: " + currentPlaceVertical);
+        }
+
+        if(currentPlaceVertical < 0){
+          currentPlaceVertical += 1;
+        }
+        else if(currentPlaceVertical > 2){
+          currentPlaceVertical -= 1;
+        }
+
+        if(currentPlaceHorizontal < 0){
+          currentPlaceHorizontal += 1;
+        }
+        else if(currentPlaceHorizontal > 2){
+          currentPlaceHorizontal -= 1;
+        }
+        System.out.println(board[currentPlaceHorizontal][currentPlaceVertical]);
+
+      }
+      System.out.println("\n\n One part done \n\n");
+      System.out.println(board[currentPlaceHorizontal][currentPlaceVertical]);
+      finalAnswer = finalAnswer + board[currentPlaceHorizontal][currentPlaceVertical];
+
+    }
+    System.out.println(finalAnswer);
 
 
 
