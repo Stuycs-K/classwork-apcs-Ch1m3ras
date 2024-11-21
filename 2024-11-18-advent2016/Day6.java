fc942ed274cca49ffae8691458b7c9be64926644
Ch1m3ras <awesomecool237@gmail.com>
2024-11-20 23:22:17 -0500

    Adapted code from day4 to create the same character checker as day4 to check the most common letter.

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -7,7 +7,9 @@ public class Day6{
 
   public static void main(String[] args){
     ArrayList<String> fileLines = new ArrayList<String>();
-    ArrayList<String> fileColumns = new ArrayList<String>(100);
+    ArrayList<String> fileColumns = new ArrayList<String>();
+    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    String answer = "";
 
     try{
       File inputFile = new File("Day6.txt");
@@ -22,9 +24,9 @@ public class Day6{
     }
 
     for(int i = 0; i < fileLines.size(); i++){
-      System.out.println(fileLines.get(i));
+      // System.out.println(fileLines.get(i));
       for(int i2 = 0; i2 < fileLines.get(i).length(); i2++){
-        System.out.println(fileLines.get(i).charAt(i2));
+        // System.out.println(fileLines.get(i).charAt(i2));
         if(i == 0){
           fileColumns.add(fileLines.get(i).charAt(i2) + "");
         }
@@ -34,8 +36,33 @@ public class Day6{
       }
     }
     for(int i = 0; i < fileColumns.size(); i++){
-      System.out.println(fileColumns.get(i));
+      //System.out.println(fileColumns.get(i));
+      int[] amountAppeared = new int[26];
+      for(int i2 = 0; i2 < fileColumns.get(i).length(); i2++){
+        for(int i3 = 0; i3 < alphabet.length; i3++){
+          if(alphabet[i3] == fileColumns.get(i).charAt(i2)){
+            amountAppeared[i3] += 1;
+          }
+        }
+      }
+      // for (int i4 = 0; i4 < amountAppeared.length; i4++){
+      //   System.out.println(alphabet[i4] + " " + amountAppeared[i4]);
+      // }
+      // System.out.println("\n\n");
+      int referenceOfHighest = 0;
+      int currentHighest = 0;
+      //System.out.println(referenceOfHighest);
+      //System.out.println(currentHighest);
+      for(int i5 = 0; i5 < amountAppeared.length; i5++){
+        if(amountAppeared[i5] > currentHighest){
+          referenceOfHighest = i5;
+          currentHighest = amountAppeared[i5];
+        }
+      }
+      answer = answer + alphabet[referenceOfHighest];
     }
+    System.out.println(answer);
+
   }
 
 }
