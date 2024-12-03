5c5a1a74fbe2c8b21fbe9954e23d393de9f740ce
Ch1m3ras <awesomecool237@gmail.com>
2024-12-03 10:05:12 -0500

    Added a basic ColorDemo idea, will expand on it at home

---
+++ 2024-12-05-colors/ColorDemo.java
@@ -0,0 +1,11 @@
+public class ColorDemo{
+
+  public static void main(String[] args){
+    String[] colors = {"\u001b[31m", "orange once i find it", "\u001b[33m", "\u001b[32m", "\u001b[34m", "pink when i find the rgb", "purple when i find the rgb"}
+    String[] letters = {"r", "a", "i", "n", "b", "o", "w"};
+
+    //Final Reset
+    System.out.println("\u001b[0m");
+  }
+
+}
