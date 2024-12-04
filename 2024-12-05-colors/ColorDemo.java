e2ca49fa400336ca60bf98737cb283c58a36fda8
Ch1m3ras <awesomecool237@gmail.com>
2024-12-03 23:39:57 -0500

    Added a rainbow that utilizes all the colors of the rainbow(although the last 3 seem just to be blue instead of being pink and purple.)

--- 2024-12-05-colors/ColorDemo.java
+++ 2024-12-05-colors/ColorDemo.java
@@ -1,9 +1,14 @@
 public class ColorDemo{
 
   public static void main(String[] args){
-    String[] colors = {"\u001b[31m", "orange once i find it", "\u001b[33m", "\u001b[32m", "\u001b[34m", "pink when i find the rgb", "purple when i find the rgb"}
+    String[] colors = {"\u001b[31m", "\u001b[ 38 ; 2 ; 255 ; 165 ; 0 m", "\u001b[33m", "\u001b[32m", "\u001b[34m", "\u001b[ 38 ; 2 ; 255 ; 192 ; 203 m", "\u001b[ 38 ; 2 ; 128 ; 0 ; 128 m"};
     String[] letters = {"r", "a", "i", "n", "b", "o", "w"};
 
+    for(int i = 0; i < colors.length; i++){
+      System.out.print(colors[i] + letters[i]);
+    }
+
+    
     //Final Reset
     System.out.println("\u001b[0m");
   }
