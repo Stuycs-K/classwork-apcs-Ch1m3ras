4c835f11c04be47ace472fbb0509c2362200bbcf
Ch1m3ras <awesomecool237@gmail.com>
2024-12-04 21:38:46 -0500

    Added the side borders to Driver function

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -4,15 +4,27 @@ public class Driver{
     int xSize = 80;
     int ySize = 30;
     System.out.println("\033[2J"); //Clears the screen
-    makeUpperAndLowerBorder(80, 30);
+    border(80, 30);
     System.out.print("\033[0m"); //Resets everything to defaults
   }
 
-  public static void makeUpperAndLowerBorder(int length, int height){
+  public static void border(int length, int height){
     Text.go(1, 1);
     for(int i = 0; i < length; i++){
       Text.color(41 + (i % 8), 31 + (i % 8));
       System.out.print("l");
+    }
+    Text.go(2, 1);
+    for(int i = 1; i < height - 1; i++){
+      Text.color(41 + (i % 8), 31 + (i % 8));
+      System.out.print("l");
+      Text.go(2 + i, 1);
+    }
+    Text.go(2, length);
+    for(int i = 1; i < height - 1; i++){
+      Text.color(41 + (i % 8), 31 + (i % 8));
+      System.out.print("l");
+      Text.go(2 + i, length);
     }
     Text.go(height, 1);
     for(int i = length; i > 0; i--){
