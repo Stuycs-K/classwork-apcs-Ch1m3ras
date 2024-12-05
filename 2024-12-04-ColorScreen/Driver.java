3f2e17c72c20e4f60be77c1fc6bd71631f7b59e2
Ch1m3ras <awesomecool237@gmail.com>
2024-12-04 21:27:45 -0500

    Added a method to draw the top and bottom borders of the 80 by 30 rectangle

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,7 +1,24 @@
 public class Driver{
 
   public static void main(String[] args){
-    System.out.println("This functions");
+    int xSize = 80;
+    int ySize = 30;
+    System.out.println("\033[2J"); //Clears the screen
+    makeUpperAndLowerBorder(80, 30);
+    System.out.print("\033[0m"); //Resets everything to defaults
+  }
+
+  public static void makeUpperAndLowerBorder(int length, int height){
+    Text.go(1, 1);
+    for(int i = 0; i < length; i++){
+      Text.color(41 + (i % 8), 31 + (i % 8));
+      System.out.print("l");
+    }
+    Text.go(height, 1);
+    for(int i = length; i > 0; i--){
+      Text.color(41 + (i % 8), 31 + (i % 8));
+      System.out.print("l");
+    }
   }
 
 }
