95688367d5fc5c1b41a551078c0afbaabf41c376
Ch1m3ras <awesomecool237@gmail.com>
2024-12-06 09:57:57 -0500

    Added the constructor along with the accessor methods and their private variables

--- 2024-12-06-Adventurer/Artificer.java
+++ 2024-12-06-Adventurer/Artificer.java
@@ -3,9 +3,38 @@ public class Artificer extends Adventurer{
   private int special;
   private int specialMax;
 
-  public Artificer(name){
-      super(name);
-
+  public Artificer(String name){
+      super(name, 10);
+      setSpecial(10);
   }
-  
+
+  public Artificer(String name, int hp){
+    super(name, hp);
+    setSpecial(10);
+  }
+
+  public Artificer(String name, int hp, int special){
+    super(name, hp);
+    setSpecial(special);
+  }
+
+  public String getSpecialName(){
+    return "Scrap";
+  }
+
+  public int getSpecial(){
+    return special;
+  }
+
+  public void setSpecial(int val){
+    if(val > specialMax){
+      specialMax = val;
+    }
+    special = val;
+  }
+
+  public int getSpecialMax(){
+    return specialMax;
+  }
+
 }
